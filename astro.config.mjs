23e75d377180ff08f6b968bacc3c4978ab84f62e
Iggy Spolski <58820214+Ignisko@users.noreply.github.com>
2024-08-18 21:42:31 +0200

    Update astro.config.mjs
    #correct changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://spol.ski/', // For local development
+    site: 'https://github.com/Ignisko/ignisko.github.io', // For local development
 });
